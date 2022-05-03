385e45cf7e470584470f95e61c4ce0fdce28a944
Vladimir Lazic <zumjzk92@gmail.com>
2022-05-03 21:35:54 +0200

    added some code

---
+++ ZdravoHospital/ZdravoHospital/ZdravoHospital/Controller/RelocateEquipmentController.cs
@@ -0,0 +1,25 @@
+// File:    RelocateEquipmentController.cs
+// Author:  User
+// Created: Tuesday, May 3, 2022 8:45:12 PM
+// Purpose: Definition of Class RelocateEquipmentController
+
+using System;
+
+namespace Controller
+{
+   public class RelocateEquipmentController
+   {
+      public List<Equipment> GetEquipment()
+      {
+         throw new NotImplementedException();
+      }
+      
+      public void RelocateEquipment(RelocateEquipment relocateEquipment)
+      {
+         throw new NotImplementedException();
+      }
+      
+      public Service.RelocateEquipmentService relocateEquipmentService;
+   
+   }
+}
